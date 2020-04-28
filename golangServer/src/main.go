712e53b91bca34dbc3ab2f8eca2f83e9d5a80d58
shahar-y <shya95@gmail.com>
2020-04-28 23:14:50 +0300

    cleared code, map is secured with async
    Signed-off-by: shahar-y <shya95@gmail.com>

--- golangServer/src/main.go
+++ golangServer/src/main.go
@@ -5,12 +5,15 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync"
 
 	"github.com/gorilla/websocket"
 )
 
-var clients = make(map[*websocket.Conn]bool) // connected clients
-var broadcast = make(chan Position)          // broadcast channel
+var sm sync.Map
+
+// var clients = make(map[*websocket.Conn]bool) // connected clients
+var broadcast = make(chan Position) // broadcast channel
 
 // Configure the upgrader
 var upgrader = websocket.Upgrader{
@@ -27,10 +30,6 @@ type Position struct {
 var pos = Position{X: 100, Y: 100}
 
 func main() {
-	// Create a simple file server
-	// fs := http.FileServer(http.Dir("../public"))
-	// http.Handle("/", fs)
-
 	// Configure websocket route
 	http.HandleFunc("/ws", handleConnections)
 
@@ -47,9 +46,7 @@ func main() {
 }
 
 func handleConnections(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("handleConnections...")
 	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
-	// setupResponse(&w, r)
 	// Upgrade initial GET request to a websocket
 	ws, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
@@ -59,58 +56,45 @@ func handleConnections(w http.ResponseWriter, r *http.Request) {
 	defer ws.Close()
 
 	// Register our new client
-	clients[ws] = true
+	sm.Store(ws, ws)
 
-	for {
-
-		// fmt.Println(ws)
-
-		reader(ws)
-		// var msg Message
-		// Read in a new message as JSON and map it to a Message object
-		// err := ws.ReadJSON(&msg)
-		// fmt.Println(msg)
-		// if err != nil {
-		// 	log.Printf("error: %v", err)
-		// 	delete(clients, ws)
-		// 	break
-		// }
-		// // Send the newly received message to the broadcast channel
-		// broadcast <- msg
-	}
+	reader(ws)
 }
 
 func reader(conn *websocket.Conn) {
 	for {
 		_, p, err := conn.ReadMessage()
 		if err != nil {
-			log.Println(err)
+			log.Println("err in readMessage!!")
+			conn.Close()
+			sm.Delete(conn)
 			return
 		}
 
 		s := string(p)
-		fmt.Println("the request:")
 		fmt.Println(s)
+
 		switch s {
 		case "a":
 			pos.X -= 5
-			// socketio.emit("position", position);
 			break
 		case "d":
 			pos.X += 5
-			// socketio.emit("position", position);
 			break
 		case "w":
 			pos.Y -= 5
-			// socketio.emit("position", position);
 			break
 		case "s":
 			pos.Y += 5
 			break
 		}
+		fmt.Println(pos)
 
 		if err := conn.WriteJSON(pos); err != nil {
 			log.Println(err)
+			conn.Close()
+			sm.Delete(conn)
+			// delete(clients, conn)
 			return
 		}
 		broadcast <- pos
@@ -121,22 +105,25 @@ func broadcastMessage() {
 	fmt.Println("handleMessages")
 
 	for {
+		i := 0
 		// Grab the next message from the broadcast channel
 		msg := <-broadcast
 		// Send it out to every client that is currently connected
-		for client := range clients {
+		sm.Range(func(ki, c interface{}) bool {
+			i++
+			fmt.Printf("i: %v\n", i)
+			client, ok := c.(*websocket.Conn)
+			if ok != true {
+				fmt.Println("NOT OK!")
+			}
 			err := client.WriteJSON(msg)
 			if err != nil {
-				log.Printf("error: %v", err)
+				log.Printf("client.WriteJSON error: %v", err)
 				client.Close()
-				delete(clients, client)
+				sm.Delete(ki)
+				// delete(clients, client)
 			}
-		}
+			return true
+		})
 	}
-}
-
-func setupResponse(w *http.ResponseWriter, req *http.Request) {
-	(*w).Header().Set("Access-Control-Allow-Origin", "*")
-	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 }
